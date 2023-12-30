37cc7b701520f756dd84295f92113bbb4e98e6bb
Mian <mzhong@ethz.ch>
2023-12-30 17:26:33 +0800

    update roxygen and description

---
+++ R/washdev.R
@@ -0,0 +1,30 @@
+#' \describe{
+#'   \item{paperid}{ID number of the paper on the journal website}
+#'   \item{volume}{Volume number of the journal}
+#'   \item{issue}{Issue number of the journal}
+#'   \item{url}{Official website url of the paper}
+#'   \item{journal}{Full name of the journal}
+#'   \item{title}{Title of the paper}
+#'   \item{published_year}{Year of publication}
+#'   \item{is_supp}{Whether the paper has supplementary materials}
+#'   \item{num_supp}{Number of supplementary material files}
+#'   \item{supp_file_type}{File type of the supplementary materials}
+#'   \item{supp_url}{Website url of the supplementary materials}
+#'   \item{num_authors}{Number of the authors}
+#'   \item{first_author_name}{Name of the first author}
+#'   \item{first_author_affiliation}{Academic affiliation of the first author}
+#'   \item{first_author_affiliation_country}{Country or region of the first author parsed from first_author_affiliation variable}
+#'   \item{first_author_email}{Email of the first author}
+#'   \item{first_author_orcid}{ORCID of the first author}
+#'   \item{correspondence_author_name}{Name of the correspondence author}
+#'   \item{correspondence_author_affiliation}{Academic affiliation of the correspondence author}
+#'   \item{correspondence_author_affiliation_country}{Country or region of the correspondence author parsed from correspondence_author_affiliation variable}
+#'   \item{correspondence_author_email}{Email of the correspondence author}
+#'   \item{correspondence_author_orcid}{ORCID of the correspondence author}
+#'   \item{has_das}{Whether the paper has a data availability statement}
+#'   \item{das}{Original data availability statement of the paper}
+#'   \item{das_type}{Type of the data availability statement #todo}
+#'   \item{das_repo_url}{Website url of the data if the relevant data of the paper is shared on a public repository}
+#'   \item{keywords}{List of keywords of the paper}
+#' }
+"washdev"
