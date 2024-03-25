e8fd799e5786a39c6c0ea27599e5bd57f928706b
Mian <mzhong@ethz.ch>
2024-03-25 14:09:07 +0100

    update method in readme and minor fixes on data-processing and dict

--- README.md
+++ README.md
@@ -66,7 +66,7 @@ washdev |>
   gt::as_raw_html()
 ```
 
-<div id="wocqyuwuhv" style="padding-left:0px;padding-right:0px;padding-top:10px;padding-bottom:10px;overflow-x:auto;overflow-y:auto;width:auto;height:auto;">
+<div id="gvspvthoan" style="padding-left:0px;padding-right:0px;padding-top:10px;padding-bottom:10px;overflow-x:auto;overflow-y:auto;width:auto;height:auto;">
   &#10;  <table class="gt_table" data-quarto-disable-processing="false" data-quarto-bootstrap="false" style="-webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; font-family: system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'; display: table; border-collapse: collapse; line-height: normal; margin-left: auto; margin-right: auto; color: #333333; font-size: 16px; font-weight: normal; font-style: normal; background-color: #FFFFFF; width: auto; border-top-style: solid; border-top-width: 2px; border-top-color: #A8A8A8; border-right-style: none; border-right-width: 2px; border-right-color: #D3D3D3; border-bottom-style: solid; border-bottom-width: 2px; border-bottom-color: #A8A8A8; border-left-style: none; border-left-width: 2px; border-left-color: #D3D3D3;" bgcolor="#FFFFFF">
   <thead style="border-style: none;">
     &#10;    <tr class="gt_col_headings" style="border-style: none; border-top-style: solid; border-top-width: 2px; border-top-color: #D3D3D3; border-bottom-style: solid; border-bottom-width: 2px; border-bottom-color: #D3D3D3; border-left-style: none; border-left-width: 1px; border-left-color: #D3D3D3; border-right-style: none; border-right-width: 1px; border-right-color: #D3D3D3;">
@@ -560,7 +560,8 @@ das_type
 factor
 </td>
 <td style="text-align:left;">
-Type of the data availability statement \#todo
+Type of the data availability statement including “in paper” “on
+request” “available in online repository” “not sharable” “NA”
 </td>
 </tr>
 <tr>
@@ -604,7 +605,7 @@ uncnewsletter |>
   gt::as_raw_html()
 ```
 
-<div id="zqjoicsssj" style="padding-left:0px;padding-right:0px;padding-top:10px;padding-bottom:10px;overflow-x:auto;overflow-y:auto;width:auto;height:auto;">
+<div id="hvjulvnumm" style="padding-left:0px;padding-right:0px;padding-top:10px;padding-bottom:10px;overflow-x:auto;overflow-y:auto;width:auto;height:auto;">
   &#10;  <table class="gt_table" data-quarto-disable-processing="false" data-quarto-bootstrap="false" style="-webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; font-family: system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'; display: table; border-collapse: collapse; line-height: normal; margin-left: auto; margin-right: auto; color: #333333; font-size: 16px; font-weight: normal; font-style: normal; background-color: #FFFFFF; width: auto; border-top-style: solid; border-top-width: 2px; border-top-color: #A8A8A8; border-right-style: none; border-right-width: 2px; border-right-color: #D3D3D3; border-bottom-style: solid; border-bottom-width: 2px; border-bottom-color: #A8A8A8; border-left-style: none; border-left-width: 2px; border-left-color: #D3D3D3;" bgcolor="#FFFFFF">
   <thead style="border-style: none;">
     &#10;    <tr class="gt_col_headings" style="border-style: none; border-top-style: solid; border-top-width: 2px; border-top-color: #D3D3D3; border-bottom-style: solid; border-bottom-width: 2px; border-bottom-color: #D3D3D3; border-left-style: none; border-left-width: 1px; border-left-color: #D3D3D3; border-right-style: none; border-right-width: 1px; border-right-color: #D3D3D3;">
@@ -1196,8 +1197,8 @@ dataset.
 
 Then, for each publication, we retrieve the needed variables from the
 publication’s html file using the publication url. The retrieval is
-rule-based to find relevant fields (e.g. supplementary materials) and
-extract the value.
+rule-based to find the relevant fields (e.g. supplementary materials)
+and extract the value.
 
 ### uncnewsletter
 
--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -9,23 +9,23 @@ washdev <- read_csv("data-raw/washdev.csv")[2:28]
 # clean country/region names ---------------------------------------------------
 ## first author
 washdev <- washdev |>
-  dplyr::mutate(first_author_affiliation_country =
-                  str_replace(string = first_author_affiliation_country,
+  dplyr::mutate(first_author_affiliation_region =
+                  str_replace(string = first_author_affiliation_region,
                               pattern = "\\s+E-mail.*",
                               replacement = "")) |>
-  dplyr::mutate(first_author_affiliation_country =
-                  str_replace(string = first_author_affiliation_country,
+  dplyr::mutate(first_author_affiliation_region =
+                  str_replace(string = first_author_affiliation_region,
                               patter = "Canada .*",
                               replacement = "Canada"))
 
 ## correspondence author
 washdev <- washdev |>
-  dplyr::mutate(correspondence_author_affiliation_country =
-                  str_replace(string = correspondence_author_affiliation_country,
+  dplyr::mutate(correspondence_author_affiliation_region =
+                  str_replace(string = correspondence_author_affiliation_region,
                               pattern = "\\s+E-mail.*",
                               replacement = "")) |>
-  dplyr::mutate(correspondence_author_affiliation_country =
-                  str_replace(string = correspondence_author_affiliation_country,
+  dplyr::mutate(correspondence_author_affiliation_region =
+                  str_replace(string = correspondence_author_affiliation_region,
                               patter = "Canada .*",
                               replacement = "Canada"))
 
@@ -36,15 +36,16 @@ washdev <- washdev |>
 
 # modify das type --------------------------------------------------------------
 washdev <- washdev |>
-  mutate(das_type = str_replace(das_type, pattern = "^All relevant data are available from.*", replacement = "available in online repository")) |>
-  mutate(das_type = str_replace(das_type, pattern = "^All relevant data used in this study are available from online repositories.*", replacement = "available in online repository")) |>
-  mutate(das_type = str_replace(das_type, pattern = ".*available on Zenodo.*", replacement = "available in online repository")) |>
-  mutate(das_type = str_replace(das_type, pattern = "^All relevant data are included in the paper.*", replacement = "in paper")) |>
-  mutate(das_type = str_replace(das_type, pattern = ".+readers should contact the corresponding author.*", replacement = "on request"))
-# modify supp type into list-column
-washdev <- washdev |>
-  mutate(supp_file_type = strsplit(supp_file_type, " & "))
+  dplyr::mutate(das_type = str_replace(das_type, pattern = "^All relevant data are available from.*", replacement = "available in online repository")) |>
+  dplyr::mutate(das_type = str_replace(das_type, pattern = "^All relevant data used in this study are available from online repositories.*", replacement = "available in online repository")) |>
+  dplyr::mutate(das_type = str_replace(das_type, pattern = ".*available on Zenodo.*", replacement = "available in online repository")) |>
+  dplyr::mutate(das_type = str_replace(das_type, pattern = "^All relevant data are included in the paper.*", replacement = "in paper")) |>
+  dplyr::mutate(das_type = str_replace(das_type, pattern = ".+readers should contact the corresponding author.*", replacement = "on request"))
 
+# modify supp type into list-column
+# washdev <- washdev |>
+#  dplyr::mutate(supp_file_type = strsplit(supp_file_type, " & "))
+# manually added misc stuff
 
 # UNCNEWSLETTER DATA -----------------------------------------------------------
 uncnewsletter <- readr::read_csv("./data-raw/unc-article-url-manual-collection.csv")
@@ -54,7 +55,7 @@ uncnewsletter <- uncnewsletter |>
   dplyr::mutate(paper_info = NULL) |>
   dplyr::filter(!is.na(title)) |>
   dplyr::mutate(supp_file_type = stringr::str_to_lower(supp_file_type)) |>
-  tidyr::replace_na(num_supp, 0) |>
+  dplyr::mutate(num_supp = tidyr::replace_na(num_supp, 0)) |>
   mutate(supp_file_type = strsplit(supp_file_type, " & "))
 
 # Write to R data object -------------------------------------------------------
